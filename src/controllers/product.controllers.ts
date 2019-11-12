4e6c54329ab3e2255c0c3c847f82a2a311acf250
Nelson Acosta <nelson.acosta.web@gmail.com>
2019-11-12 12:27:24 -0300

    Añadido ruta de prueba Fix #3

--- src/controllers/product.controllers.ts
+++ src/controllers/product.controllers.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from 'express';
 
-export async function helloWorld(req: Request, res: Response): Promise<Response>{
+export async function helloWorld(req: Request, res: Response): Promise<Response> {
     return await res.json({
-        message: "Hello World"
+        message: "Test Route"
     });
 }
